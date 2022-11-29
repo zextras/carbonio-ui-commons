2374fc244b81808075f5f66d7801839dc3735ec6
gabriele <gabriele.marino@zextras.com>
2022-11-29 11:26:55 +0100

    fix: get prefs exports all prefs

--- utils/get-prefs.ts
+++ utils/get-prefs.ts
@@ -3,30 +3,5 @@ import { UserPrefs } from '../types';
 
 export const getPrefs = (): UserPrefs => {
 	const { prefs } = getUserSettings();
-	const {
-		zimbraPrefDefaultCalendarId,
-		zimbraPrefCalendarDefaultApptDuration,
-		zimbraPrefCalendarWorkingHours,
-		zimbraPrefTimeZoneId,
-		zimbraPrefCalendarFirstDayOfWeek,
-		zimbraPrefLocale,
-		zimbraPrefCalendarInitialView,
-		zimbraPrefCalendarApptReminderWarningTime,
-		zimbraPrefUseTimeZoneListInCalendar,
-		zimbraPrefIncludeTrashInSearch,
-		zimbraPrefIncludeSharedItemsInSearch
-	} = prefs;
-	return <UserPrefs>{
-		zimbraPrefDefaultCalendarId,
-		zimbraPrefCalendarDefaultApptDuration,
-		zimbraPrefCalendarWorkingHours,
-		zimbraPrefTimeZoneId,
-		zimbraPrefCalendarFirstDayOfWeek,
-		zimbraPrefLocale,
-		zimbraPrefCalendarInitialView,
-		zimbraPrefCalendarApptReminderWarningTime,
-		zimbraPrefUseTimeZoneListInCalendar,
-		zimbraPrefIncludeTrashInSearch,
-		zimbraPrefIncludeSharedItemsInSearch
-	};
+	return <UserPrefs>prefs;
 };

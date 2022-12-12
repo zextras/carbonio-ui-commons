bc3955a3243aa42efd3294a42f24ee47feb122a2
Luca Stauble <luca.stauble@zextras.com>
2022-12-12 16:56:36 +0100

    chore: merge from main

--- test/test-setup.tsx
+++ test/test-setup.tsx
@@ -4,8 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import React, { useMemo } from 'react';
-
-// import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks';
 import { render, RenderResult } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { ModalManager, ThemeProvider, SnackbarManager } from '@zextras/carbonio-design-system';
@@ -13,7 +12,6 @@ import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
 import { MemoryRouter, Route } from 'react-router-dom';
 import { Store } from 'redux';
-// import { useOnClickNewButton } from '../../hooks/on-click-new-button';
 import I18nTestFactory from './i18n/i18n-test-factory';
 import { previewContextMock, PreviewsManagerContext } from './mocks/carbonio-ui-preview';
 
@@ -59,6 +57,36 @@ function customRender(ui: React.ReactElement, options: any): RenderResult {
 	});
 }
 
+// function customRender(ui: React.ReactElement, options?: any): RenderResult {
+// 	const ProvidersWrapper = ({ children }: ProvidersWrapperProps): JSX.Element => {
+// 		const { store = {}, initialEntries = ['/'], path = '/' } = options;
+//
+// 		const i18n = useMemo(() => {
+// 			const i18nFactory = new I18nTestFactory();
+// 			return i18nFactory.getAppI18n();
+// 		}, []);
+//
+// 		return (
+// 			<ThemeProvider>
+// 				<Provider store={store}>
+// 					<I18nextProvider i18n={i18n}>
+// 						<SnackbarManager>
+// 							<PreviewsManagerContext.Provider value={previewContextMock}>
+// 								<ModalManager>{children}</ModalManager>
+// 							</PreviewsManagerContext.Provider>
+// 						</SnackbarManager>
+// 					</I18nextProvider>
+// 				</Provider>
+// 			</ThemeProvider>
+// 		);
+// 	};
+//
+// 	return render(ui, {
+// 		wrapper: ProvidersWrapper,
+// 		...options
+// 	});
+// }
+
 export function setupTest(
 	...args: Parameters<typeof customRender>
 ): { user: ReturnType<typeof userEvent['setup']> } & ReturnType<typeof render> {
@@ -67,18 +95,19 @@ export function setupTest(
 		...customRender(...args)
 	};
 }
+
 type Options = {
 	initialEntries?: Array<string>;
 	path?: string;
 	store?: Store;
 };
 
-// // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-// export function setupHook(hook: any, options: Options = {}): any {
-// 	const Wrapper = ({ children }: ProvidersWrapperProps): JSX.Element => (
-// 		<ProvidersWrapper options={options}>{children}</ProvidersWrapper>
-// 	);
-// 	const { result, unmount } = renderHook(() => useOnClickNewButton(), { wrapper: Wrapper });
-//
-// 	return { result, unmount };
-// }
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function setupHook(hook: any, options: Options = {}): any {
+	const Wrapper = ({ children }: ProvidersWrapperProps): JSX.Element => (
+		<ProvidersWrapper options={options}>{children}</ProvidersWrapper>
+	);
+	const { result, unmount } = renderHook(() => hook, { wrapper: Wrapper });
+
+	return { result, unmount };
+}

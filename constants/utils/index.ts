3712723cf007b79a282b61b069dd067b1599e7af
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2022-12-20 15:49:47 +0100

    chore: progress save

---
+++ constants/utils/index.ts
@@ -0,0 +1,14 @@
+export const FOLDER_VIEW = {
+	search_folder: 'search folder',
+	tag: 'tag',
+	conversation: 'conversation',
+	message: 'message',
+	contact: 'contact',
+	document: 'document',
+	appointment: 'appointment',
+	virtual_conversation: 'virtual conversation',
+	remote_folder: 'remote folder',
+	wiki: 'wiki',
+	task: 'task',
+	chat: 'chat'
+};

b7d6aadea0b5c0499a161c29be2a194f774b1ce3
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2022-12-21 12:36:08 +0100

    chore: progress save

--- types/tags/index.d.ts
+++ types/tags/index.d.ts
@@ -1,5 +1,45 @@
-import { ComponentType, SyntheticEvent } from 'react';
-import { ItemType } from '../../../types';
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import React, { ComponentType, SyntheticEvent } from 'react';
+import { ItemType as AccordionItemType } from '@zextras/carbonio-design-system';
+import { Tag } from '@zextras/carbonio-shell-ui';
+
+export type ReturnType = {
+	id: string;
+	icon: string;
+	label: string;
+	click?: (arg: React.SyntheticEvent<HTMLElement, Event> | KeyboardEvent) => void;
+	items?: Array<{
+		customComponent: ComponentType;
+		id: string;
+		icon: string;
+		label: string;
+	}>;
+};
+
+export type TagsFromStoreType = Record<string, Tag>;
+
+export type ArgumentType = {
+	createModal?: (...args: any) => () => void;
+	createSnackbar?: (...args: any) => void;
+	items?: ReturnType;
+	tag?: ItemType;
+};
+
+export interface ItemType extends AccordionItemType<T> {
+	item: T;
+	CustomComponent?: ComponentType<T>;
+	active: boolean;
+	color: number;
+	id: string;
+	label: string;
+	name: string;
+	open: boolean;
+	actions?: Array<unknown>;
+}
 
 export type TagsAccordionItems = {
 	items: ItemType[];

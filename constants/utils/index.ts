3b91503b52fae87055410039840631a2011969be
Giuliano <75381134+giuliano176@users.noreply.github.com>
2023-02-15 11:50:17 +0100

    feat: added sidebar-mui component (#21)
    * feat: added sidebar-mui component
    * feat: added sidebar functions, types and components

---
+++ constants/utils/index.ts
@@ -0,0 +1,28 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+export const FOLDER_VIEW = {
+	search_folder: 'search folder',
+	tag: 'tag',
+	conversation: 'conversation',
+	message: 'message',
+	contact: 'contact',
+	document: 'document',
+	appointment: 'appointment',
+	virtual_conversation: 'virtual conversation',
+	remote_folder: 'remote folder',
+	wiki: 'wiki',
+	task: 'task',
+	chat: 'chat'
+};
+
+// eslint-disable-next-line no-shadow
+export enum TagsActionsType {
+	NEW = 'new',
+	DELETE = 'delete',
+	EDIT = 'edit',
+	Apply = 'apply'
+}

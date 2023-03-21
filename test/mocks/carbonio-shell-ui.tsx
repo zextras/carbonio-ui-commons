26814ec0e43ccb9971ab82b4fb568bbe399d2337
Luca Stauble <luca.stauble@zextras.com>
2023-03-21 09:20:39 +0100

    Add new roots functions mocks (#32)
    * test: getRoot and getRoots mocks added

--- test/mocks/carbonio-shell-ui.tsx
+++ test/mocks/carbonio-shell-ui.tsx
@@ -6,8 +6,6 @@
 
 import React from 'react';
 import { generateAccount } from './accounts/account-generator';
-import { getMockedAccountItem } from './accounts/fakeAccounts';
-import { roots } from './folders/roots';
 import { generateRoots } from './folders/roots-generator';
 import { getSoapFetch } from './network/fetch';
 import { generateSettings } from './settings/settings-generator';
@@ -78,7 +76,9 @@ export const getIntegratedFunction = jest.fn(() => [
 export const useBoard = jest.fn();
 export const getBridgedFunctions = jest.fn();
 export const useRoot = jest.fn((id: string) => mockedRoots[id]);
+export const getRoot = jest.fn((id: string) => mockedRoots[id]);
 export const useRoots = jest.fn(() => mockedRoots);
+export const getRoots = jest.fn(() => mockedRoots);
 export const useFolders = jest.fn();
 export const addBoard = jest.fn();
 export const useBoardHooks = jest.fn();

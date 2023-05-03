7e1ba97d8fa9bb4ed28d7e7e8e744f2a16543725
Giuliano <75381134+giuliano176@users.noreply.github.com>
2023-05-03 16:32:41 +0200

    Update index.ts

--- constants/utils/index.ts
+++ constants/utils/index.ts
@@ -5,18 +5,18 @@
  */
 
 export const FOLDER_VIEW = {
-	search_folder = 'search folder',
-	tag = 'tag',
-	conversation = 'conversation',
-	message = 'message',
-	contact = 'contact',
-	document = 'document',
-	appointment = 'appointment',
-	virtual_conversation = 'virtual conversation',
-	remote_folder = 'remote folder',
-	wiki = 'wiki',
-	task = 'task',
-	chat = 'chat'
+	search_folder: 'search folder',
+	tag: 'tag',
+	conversation: 'conversation',
+	message: 'message',
+	contact: 'contact',
+	document: 'document',
+	appointment: 'appointment',
+	virtual_conversation: 'virtual conversation',
+	remote_folder: 'remote folder',
+	wiki: 'wiki',
+	task: 'task',
+	chat: 'chat'
 } as const;
 
 // eslint-disable-next-line no-shadow

82c7d5dc2ec927b88fb51216897bbcc05f693f4d
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2023-05-04 18:00:58 +0200

    feat: use new hooks imported from commons

--- hooks/use-folders-controller.ts
+++ hooks/use-folders-controller.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { useNotify, useRefresh } from '@zextras/carbonio-shell-ui';
-import { filter, forEach, map } from 'lodash';
+import { filter, forEach, map, reject } from 'lodash';
 import { useEffect } from 'react';
 import { getFolderRequest } from '../soap/get-folder';
 import { getShareInfoRequest } from '../soap/get-share-info';
@@ -14,10 +14,15 @@ import { folderWorker } from '../worker';
 
 const getFoldersByAccounts = async (sharedAccounts: unknown[], view: FolderView): Promise<any> =>
 	Promise.all(
-		map(sharedAccounts, async ({ ownerEmail }) => {
+		map(sharedAccounts, async ({ ownerEmail }: { ownerEmail: string }) => {
 			const response = await getFolderRequest({ view }, ownerEmail);
 			if (response?.folder?.length) {
-				return { ...response.folder[0], oname: response.folder[0].name, owner: ownerEmail };
+				return {
+					...response.folder[0],
+					oname: response.folder[0].name,
+					owner: ownerEmail,
+					name: ownerEmail
+				};
 			}
 			return response;
 		})
@@ -28,16 +33,17 @@ export const useFoldersController = (view: FolderView): void => {
 	const notify = useNotify();
 
 	useEffect(() => {
-		if (refresh) {
+		if (refresh && view) {
 			getFolderRequest({ view }).then((rootFolders: { folder: any }) => {
 				getShareInfoRequest().then((sharedFolders: { folders: any }) => {
 					if (sharedFolders?.folders) {
 						const sharedAccounts = filter(sharedFolders.folders, ['folderId', 1]);
 						getFoldersByAccounts(sharedAccounts, view).then((response) => {
+							const filteredLinks = reject(rootFolders.folder[0].link, ['rid', 1]);
 							const folders = [
 								{
 									...rootFolders.folder[0],
-									link: [...rootFolders.folder[0].link, ...response]
+									link: [...filteredLinks, ...response]
 								}
 							];
 							folderWorker.postMessage({

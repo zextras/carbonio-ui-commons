838e11c6f142b3884ecc20f92476b6acf37fa165
gabriele <gabriele.marino@zextras.com>
2023-05-19 19:20:16 +0200

    chore: code clean

--- types/folder/index.d.ts
+++ types/folder/index.d.ts
@@ -28,7 +28,7 @@ export type Searches = { [id: string]: SearchFolder };
 export type FolderState = {
 	folders: Folders;
 	searches: Searches;
-	updateFolder: (id: string, opt: any) => void;
+	updateFolder: (id: string, opt: Partial<Folder>) => void;
 };
 
 export type FolderView =

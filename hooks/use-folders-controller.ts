7e9aa5e631cf77fcc042e541234f5a9ffc61755c
Gabriele Marino <62339254+nubsthead@users.noreply.github.com>
2023-05-24 10:57:17 +0200

    fix: manage folders notify by view
    fix: manage folders notify by view

--- hooks/use-folders-controller.ts
+++ hooks/use-folders-controller.ts
@@ -3,9 +3,9 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import { useNotify, useRefresh } from '@zextras/carbonio-shell-ui';
-import { filter, forEach, map, reject } from 'lodash';
-import { useEffect } from 'react';
+import { useNotify } from '@zextras/carbonio-shell-ui';
+import { filter, forEach, isEmpty, map, reject, sortBy } from 'lodash';
+import { useEffect, useState } from 'react';
 import { getFolderRequest } from '../soap/get-folder';
 import { getShareInfoRequest } from '../soap/get-share-info';
 import { useFolderStore } from '../store/zustand/folder';
@@ -29,43 +29,64 @@ const getFoldersByAccounts = async (sharedAccounts: unknown[], view: FolderView)
 	);
 
 export const useFoldersController = (view: FolderView): void => {
-	const refresh = useRefresh();
+	const [initializing, setInitializing] = useState(true);
+	const [seq, setSeq] = useState(-1);
+
 	const notify = useNotify();
 
 	useEffect(() => {
-		if (refresh && view) {
-			getFolderRequest({ view }).then((rootFolders: { folder: any }) => {
-				getShareInfoRequest().then((sharedFolders: { folders: any }) => {
-					if (sharedFolders?.folders) {
-						const sharedAccounts = filter(sharedFolders.folders, ['folderId', 1]);
-						getFoldersByAccounts(sharedAccounts, view).then((response) => {
-							const filteredLinks = reject(rootFolders.folder[0].link, ['rid', 1]);
-							const folders = [
-								{
-									...rootFolders.folder[0],
-									link: [...filteredLinks, ...response]
-								}
-							];
-							folderWorker.postMessage({
-								op: 'refresh',
-								folder: folders ?? []
+		if (initializing && view) {
+			setInitializing((previous) => !previous);
+			getFolderRequest({ view })
+				.then((rootFolders: { folder: any }) => {
+					getShareInfoRequest().then((sharedFolders: { folders: any }) => {
+						if (sharedFolders?.folders) {
+							const sharedAccounts = filter(sharedFolders.folders, ['folderId', 1]);
+							getFoldersByAccounts(sharedAccounts, view).then((response) => {
+								const filteredLinks = reject(rootFolders.folder[0].link, ['rid', 1]);
+								const folders = [
+									{
+										...rootFolders.folder[0],
+										link: [...filteredLinks, ...response]
+									}
+								];
+								folderWorker.postMessage({
+									op: 'refresh',
+									currentView: view,
+									folder: folders ?? []
+								});
 							});
-						});
-					}
+						}
+					});
+				})
+				.catch(() => {
+					setInitializing(true);
 				});
-			});
 		}
-	}, [refresh, view]);
+	}, [initializing, view]);
 
 	useEffect(() => {
-		if (notify?.length) {
-			forEach(notify, (item) => {
-				folderWorker.postMessage({
-					op: 'notify',
-					notify: item,
-					state: useFolderStore.getState().folders
-				});
+		if (!initializing && notify.length > 0) {
+			forEach(sortBy(notify, 'seq'), (item) => {
+				if (!isEmpty(notify) && (item.seq > seq || (seq > 1 && item.seq === 1))) {
+					const isNotifyRelatedToFolders =
+						!isEmpty(notify) &&
+						(item?.created?.folder ||
+							item?.modified?.folder ||
+							item.deleted ||
+							item?.created?.link ||
+							item?.modified?.link);
+
+					if (isNotifyRelatedToFolders) {
+						folderWorker.postMessage({
+							op: 'notify',
+							notify: item,
+							state: useFolderStore.getState().folders
+						});
+					}
+					setSeq(item.seq);
+				}
 			});
 		}
-	}, [notify]);
+	}, [initializing, notify, seq, view]);
 };

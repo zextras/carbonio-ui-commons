a4495c0f50f3f793656888f3bbe5ce84bdbec323
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2023-07-11 15:39:22 +0200

    feat: updated eslint rules to work with configs v 0.1.12

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,13 +24,57 @@ module.exports = {
 		}
 	],
 	rules: {
+		'import/order': [
+			'warn',
+			{
+				groups: [['builtin', 'external']],
+				pathGroups: [
+					{
+						pattern: 'react',
+						group: 'external',
+						position: 'before'
+					}
+				],
+				pathGroupsExcludedImportTypes: ['react'],
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true
+				}
+			}
+		],
 		'no-console': ['error', { allow: ['error', 'warn'] }],
 		'notice/notice': [
 			'error',
 			{
 				templateFile: 'src/carbonio-ui-commons/notice.template.ts'
 			}
-		]
+		],
+		// sonar lint rules
+		'sonarjs/cognitive-complexity': 'warn',
+		'sonarjs/no-collapsible-if': 'warn',
+		'sonarjs/no-duplicate-string': 'warn',
+		'sonarjs/no-duplicated-branches': 'warn',
+		'sonarjs/no-identical-conditions': 'warn',
+		'sonarjs/no-identical-expressions': 'warn',
+		'sonarjs/no-redundant-boolean': 'warn',
+		'sonarjs/no-small-switch': 'warn',
+		'sonarjs/no-unused-collection': 'warn',
+		'sonarjs/no-use-of-empty-return-value': 'warn',
+		'sonarjs/prefer-immediate-return': 'warn',
+		'sonarjs/prefer-object-literal': 'warn',
+		'sonarjs/prefer-single-boolean-return': 'warn',
+		'sonarjs/prefer-while': 'warn',
+		'sonarjs/no-useless-catch': 'warn',
+		'sonarjs/no-nested-template-literals': 'warn',
+		'sonarjs/no-all-duplicated-branches': 'warn',
+		'@typescript-eslint/no-shadow': 'warn',
+		'no-param-reassign': [
+			'warn',
+			{ props: true, ignorePropertyModificationsFor: ['accumulator', 'state', 'event'] }
+		],
+		'unused-imports/no-unused-imports': 'warn'
+
 		// 'import/order': [
 		// 	'error',
 		// 	{

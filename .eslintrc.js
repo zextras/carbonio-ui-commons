07570ebed5fe2865da5d664ab2c2012624fb0339
Giuliano <75381134+giuliano176@users.noreply.github.com>
2023-07-12 09:24:52 +0200

    Update eslintrc rules (#68)
    * feat: updated eslint rules to work with configs v 0.1.12 in all modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,37 +24,62 @@ module.exports = {
 		}
 	],
 	rules: {
+		'import/order': [
+			'warn',
+			{
+				groups: [['builtin', 'external']],
+				pathGroups: [
+					{
+						pattern: 'react',
+						group: 'external',
+						position: 'before'
+					}
+				],
+				pathGroupsExcludedImportTypes: ['react'],
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true
+				}
+			}
+		],
 		'no-console': ['error', { allow: ['error', 'warn'] }],
 		'notice/notice': [
 			'error',
 			{
 				templateFile: 'src/carbonio-ui-commons/notice.template.ts'
 			}
-		]
-		// 'import/order': [
-		// 	'error',
-		// 	{
-		// 		groups: [['builtin', 'external']],
-		// 		pathGroups: [
-		// 			{
-		// 				pattern: 'react',
-		// 				group: 'external',
-		// 				position: 'before'
-		// 			}
-		// 		],
-		// 		pathGroupsExcludedImportTypes: ['react'],
-		// 		'newlines-between': 'always',
-		// 		alphabetize: {
-		// 			order: 'asc',
-		// 			caseInsensitive: true
-		// 		}
-		// 	}
-		// ],
-		// 'unused-imports/no-unused-imports': 'error',
-		// 'unused-imports/no-unused-vars': [
-		// 	'warn',
-		// 	{ vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
-		// ]
+		],
+		// sonar lint rules
+		'sonarjs/cognitive-complexity': 'warn',
+		'sonarjs/no-collapsible-if': 'warn',
+		'sonarjs/no-duplicate-string': 'warn',
+		'sonarjs/no-duplicated-branches': 'warn',
+		'sonarjs/no-identical-conditions': 'warn',
+		'sonarjs/no-identical-expressions': 'warn',
+		'sonarjs/no-redundant-boolean': 'warn',
+		'sonarjs/no-small-switch': 'warn',
+		'sonarjs/no-unused-collection': 'warn',
+		'sonarjs/no-use-of-empty-return-value': 'warn',
+		'sonarjs/prefer-immediate-return': 'warn',
+		'sonarjs/prefer-object-literal': 'warn',
+		'sonarjs/prefer-single-boolean-return': 'warn',
+		'sonarjs/prefer-while': 'warn',
+		'sonarjs/no-useless-catch': 'warn',
+		'sonarjs/no-nested-template-literals': 'warn',
+		'sonarjs/no-all-duplicated-branches': 'warn',
+		'sonarjs/no-gratuitous-expressions': 'warn',
+		'sonarjs/max-switch-cases': 'warn',
+		'sonarjs/no-empty-collection': 'warn',
+		'sonarjs/no-identical-functions': 'warn',
+
+		'@typescript-eslint/no-shadow': 'warn',
+
+		'no-param-reassign': [
+			'warn',
+			{ props: true, ignorePropertyModificationsFor: ['accumulator', 'state', 'event'] }
+		],
+		'unused-imports/no-unused-imports': 'warn'
 	},
 	settings: {
 		'import/resolver': {

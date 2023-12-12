659fd96cf9429e6ce009dec9e0496ce4a52c366c
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2023-12-12 11:59:43 +0100

    feat: add screen definition with new getBy options

---
+++ test/utils/utils.ts
@@ -0,0 +1,66 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import {
+	ByRoleMatcher,
+	ByRoleOptions,
+	GetAllBy,
+	queries,
+	queryHelpers,
+	screen as rtlScreen,
+	within as rtlWithin,
+	type Screen
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { filter } from 'lodash';
+
+export type UserEvent = ReturnType<(typeof userEvent)['setup']>;
+
+type ByRoleWithIconOptions = ByRoleOptions & {
+	icon: string | RegExp;
+};
+/**
+ * Matcher function to search an icon button through the icon data-testid
+ */
+const queryAllByRoleWithIcon: GetAllBy<[ByRoleMatcher, ByRoleWithIconOptions]> = (
+	container,
+	role,
+	{ icon, ...options }
+) =>
+	filter(
+		rtlWithin(container).queryAllByRole(role, options),
+		(element) => rtlWithin(element).queryByTestId(icon) !== null
+	);
+const getByRoleWithIconMultipleError = (
+	container: Element | null,
+	role: ByRoleMatcher,
+	options: ByRoleWithIconOptions
+): string => `Found multiple elements with role ${role} and icon ${options.icon}`;
+const getByRoleWithIconMissingError = (
+	container: Element | null,
+	role: ByRoleMatcher,
+	options: ByRoleWithIconOptions
+): string => `Unable to find an element with role ${role} and icon ${options.icon}`;
+
+const [getByRoleWithIcon] = queryHelpers.buildQueries<[ByRoleMatcher, ByRoleWithIconOptions]>(
+	queryAllByRoleWithIcon,
+	getByRoleWithIconMultipleError,
+	getByRoleWithIconMissingError
+);
+
+const customQueries = {
+	getByRoleWithIcon
+};
+
+const queriesExtended = { ...queries, ...customQueries };
+
+export function within(
+	element: Parameters<typeof rtlWithin<typeof queriesExtended>>[0]
+): ReturnType<typeof rtlWithin<typeof queriesExtended>> {
+	return rtlWithin(element, queriesExtended);
+}
+
+export const screen: Screen<typeof queriesExtended> = { ...rtlScreen, ...within(document.body) };

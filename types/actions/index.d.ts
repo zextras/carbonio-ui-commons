bc9968dc4d00d2c427f5f6b9c74d6ced72cd7160
Giuliano <75381134+giuliano176@users.noreply.github.com>
2024-02-15 16:53:57 +0100

    Iris 5013 import contacts (#101)
    * chore: add FolderAction type and rename old FolderAction to SoapFolderAction

---
+++ types/actions/index.d.ts
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+export type FolderAction = {
+	id: string;
+	icon: string;
+	label: string;
+	onClick: (ev: React.SyntheticEvent) => void;
+	disabled?: boolean;
+};

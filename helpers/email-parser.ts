f33a2de1fea53e6fa97e0a523804ce3211d9653d
Francesco Gottardi <37839534+Gammax92@users.noreply.github.com>
2024-06-26 16:06:36 +0200

    feat: add email parser helpers (#131)

---
+++ helpers/email-parser.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+export function parseEmail(input: string): string | undefined {
+	const extracted = extractEmail(input);
+	return isValidEmail(extracted) ? extracted : undefined;
+}
+
+export function isValidEmail(email: string | undefined): boolean {
+	// eslint-disable-next-line max-len, prettier/prettier, no-useless-escape
+	const validEmailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return validEmailRegex.test(String(email).toLowerCase());
+}
+
+function extractEmail(input: string): string {
+	const trimmedInput = input.trim();
+	const caputured = /.*<(.*)>/.exec(trimmedInput);
+	if (caputured && caputured.length > 1) {
+		return caputured[1].trim();
+	}
+	return trimmedInput;
+}

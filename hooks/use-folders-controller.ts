e3e392f175f4d701a9919aff4cd71ea7959f08ab
Keshav Bhatt <keshavnrj@gmail.com>
2024-08-29 14:02:06 +0530

    wip: fix looping of getFolderRequest in case of failure

--- hooks/use-folders-controller.ts
+++ hooks/use-folders-controller.ts
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { useNotify } from '@zextras/carbonio-shell-ui';
 import { filter, forEach, isEmpty, map, reject, sortBy } from 'lodash';
@@ -11,87 +11,88 @@ import { filter, forEach, isEmpty, map, reject, sortBy } from 'lodash';
 import { getFolderRequest } from '../soap/get-folder';
 import { getShareInfoRequest } from '../soap/get-share-info';
 import { useFolderStore } from '../store/zustand/folder';
-import { FolderView } from '../types/folder';
+import { FolderView } from '../types';
 import { folderWorker } from '../worker';
 
-const getFoldersByAccounts = async (sharedAccounts: unknown[], view: FolderView): Promise<any> =>
-	Promise.all(
-		map(sharedAccounts, async ({ ownerEmail }: { ownerEmail: string }) => {
-			const response = await getFolderRequest({ view }, ownerEmail);
-			if (response?.folder?.length) {
-				return {
-					...response.folder[0],
-					oname: response.folder[0].name,
-					owner: ownerEmail,
-					name: ownerEmail
-				};
-			}
-			return response;
-		})
-	);
+const getFoldersByAccounts = async (sharedAccounts: unknown[], view: FolderView): Promise<any> => {
+	try {
+		return await Promise.all(
+			map(sharedAccounts, async ({ ownerEmail }: { ownerEmail: string }) => {
+				const response = await getFolderRequest({ view }, ownerEmail);
+				if (response?.folder?.length) {
+					return {
+						...response.folder[0],
+						oname: response.folder[0].name,
+						owner: ownerEmail,
+						name: ownerEmail
+					};
+				}
+				return response;
+			})
+		);
+	} catch (error) {
+		console.error('Error fetching folders by accounts:', error);
+		return [];
+	}
+};
 
 export const useFoldersController = (view: FolderView): null => {
-	const [initializing, setInitializing] = useState(true);
+	const isLoading = useRef(false);
 	const [seq, setSeq] = useState(-1);
 
 	const notify = useNotify();
 
 	useEffect(() => {
-		if (initializing && view) {
-			setInitializing((previous) => !previous);
-			getFolderRequest({ view })
-				.then((rootFolders: { folder: any }) => {
-					getShareInfoRequest().then((sharedFolders) => {
-						if (sharedFolders?.folders) {
-							const sharedAccounts = filter(sharedFolders.folders, ['folderId', 1]);
-							if (sharedAccounts.length) {
-								const filteredLinks = reject(rootFolders.folder[0].link, ['rid', 1]);
-								getFoldersByAccounts(sharedAccounts, view).then((response) => {
-									if (!response.Fault) {
-										const folders = [
-											{
-												...rootFolders.folder[0],
-												link: filteredLinks
-											},
-											...response
-										];
-										folderWorker.postMessage({
-											op: 'refresh',
-											currentView: view,
-											folder: folders ?? []
-										});
-									} else {
-										const folders = [
-											{
-												...rootFolders.folder[0],
-												link: filteredLinks
-											}
-										];
-										folderWorker.postMessage({
-											op: 'refresh',
-											currentView: view,
-											folder: folders ?? []
-										});
-									}
-								});
-							} else {
-								folderWorker.postMessage({
-									op: 'refresh',
-									currentView: view,
-									folder: rootFolders?.folder ?? []
-								});
-							}
-						}
+		const fetchData = async (): Promise<void> => {
+			try {
+				isLoading.current = true;
+				const rootFolders = await getFolderRequest({ view });
+				const sharedFolders = await getShareInfoRequest();
+				if (sharedFolders?.folders) {
+					const sharedAccounts = filter(sharedFolders.folders, ['folderId', 1]);
+					const filteredLinks = reject(rootFolders.folder[0].link, ['rid', 1]);
+
+					const folders = sharedAccounts.length
+						? [
+								{
+									...rootFolders.folder[0],
+									link: filteredLinks
+								},
+								...(await getFoldersByAccounts(sharedAccounts, view))
+							]
+						: [
+								{
+									...rootFolders.folder[0],
+									link: filteredLinks
+								}
+							];
+
+					folderWorker.postMessage({
+						op: 'refresh',
+						currentView: view,
+						folder: folders ?? []
 					});
-				})
-				.catch(() => {
-					setInitializing(true);
-				});
+				} else {
+					folderWorker.postMessage({
+						op: 'refresh',
+						currentView: view,
+						folder: rootFolders?.folder ?? []
+					});
+				}
+			} catch (error) {
+				console.error('Error in fetching folder data:', error);
+			} finally {
+				isLoading.current = false;
+			}
+		};
+
+		if (!isLoading.current && view) {
+			fetchData();
 		}
-	}, [initializing, view]);
+	}, [isLoading, view]);
 
 	useEffect(() => {
-		if (!initializing && notify.length > 0) {
+		if (!isLoading.current && notify.length > 0) {
 			forEach(sortBy(notify, 'seq'), (item) => {
 				if (!isEmpty(notify) && (item.seq > seq || (seq > 1 && item.seq === 1))) {
 					const isNotifyRelatedToFolders =
@@ -113,6 +114,6 @@ export const useFoldersController = (view: FolderView): null => {
 				}
 			});
 		}
-	}, [initializing, notify, seq, view]);
+	}, [isLoading, notify, seq, view]);
 	return null;
 };

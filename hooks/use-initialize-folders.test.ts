60171770b24f1385b2f80218263c485815949b0b
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-08-29 16:42:21 +0200

    test(useInitializeFolders): remove unused tests

--- hooks/use-initialize-folders.test.ts
+++ hooks/use-initialize-folders.test.ts
@@ -9,7 +9,6 @@ import { http, HttpResponse } from 'msw';
 import { useInitializeFolders } from './use-initialize-folders';
 import { useFolderStore } from '../store/zustand/folder';
 import { getSetupServer } from '../test/jest-setup';
-import * as shell from '../test/mocks/carbonio-shell-ui';
 import { handleGetFolderRequest } from '../test/mocks/network/msw/handle-get-folder';
 import {
 	getEmptyMSWShareInfoResponse,
@@ -18,13 +17,6 @@ import {
 import { setupHook } from '../test/test-setup';
 import { FolderView } from '../types/folder';
 import { folderWorker } from '../worker';
-
-const getDifferentViewCreation = (view: FolderView): unknown => {
-	if (view === 'appointment' || view === 'contact') {
-		return { m: [] };
-	}
-	return { appt: {} };
-};
 
 describe.each<FolderView>(['appointment', 'message', 'contact'])('with %s parameter', (view) => {
 	test('on first render it will call refresh', async () => {
@@ -39,54 +31,7 @@ describe.each<FolderView>(['appointment', 'message', 'contact'])('with %s parame
 			expect.objectContaining({ op: 'refresh', currentView: view, folder: expect.any(Object) })
 		);
 	});
-	test('on rerender it will not call refresh but will call notify', async () => {
-		useFolderStore.setState({ folders: {} });
-		const notify = { deleted: ['1'], seq: 2 };
-		const workerSpy = jest.spyOn(folderWorker, 'postMessage');
-		getSetupServer().use(http.post('/service/soap/GetFolderRequest', handleGetFolderRequest));
-		getSetupServer().use(http.post('/service/soap/GetShareInfoRequest', handleGetShareInfoRequest));
 
-		shell.useNotify.mockReturnValueOnce([]).mockReturnValueOnce([notify]);
-		const { rerender } = await waitFor(() =>
-			setupHook(useInitializeFolders, { initialProps: [view] })
-		);
-		rerender();
-
-		expect(workerSpy).toHaveBeenCalled();
-		expect(workerSpy).toHaveBeenCalledTimes(2);
-		expect(workerSpy).not.toHaveBeenCalledWith(undefined);
-		expect(workerSpy).toHaveBeenCalledWith(
-			expect.objectContaining({ op: 'refresh', currentView: view, folder: expect.any(Object) })
-		);
-		expect(workerSpy).toHaveBeenCalledWith(
-			expect.objectContaining({ op: 'notify', notify, state: expect.any(Object) })
-		);
-	});
-	test('if notify is related to another view it wont call the notify', async () => {
-		useFolderStore.setState({ folders: {} });
-		const notify = { created: getDifferentViewCreation(view as FolderView), seq: 2 };
-		const workerSpy = jest.spyOn(folderWorker, 'postMessage');
-		getSetupServer().use(http.post('/service/soap/GetFolderRequest', handleGetFolderRequest));
-		getSetupServer().use(http.post('/service/soap/GetShareInfoRequest', handleGetShareInfoRequest));
-
-		shell.useNotify.mockReturnValueOnce([]).mockReturnValueOnce([
-			// SoapNotify type inside shell is incomplete
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			notify
-		]);
-		const { rerender } = await waitFor(() =>
-			setupHook(useInitializeFolders, { initialProps: [view] })
-		);
-		rerender();
-
-		expect(workerSpy).toHaveBeenCalled();
-		expect(workerSpy).toHaveBeenCalledTimes(1);
-		expect(workerSpy).not.toHaveBeenCalledWith(undefined);
-		expect(workerSpy).not.toHaveBeenCalledWith(
-			expect.objectContaining({ op: 'notify', notify, state: expect.any(Object) })
-		);
-	});
 	test('If multiple accounts are available they will be on the same level of the main account', async () => {
 		useFolderStore.setState({ folders: {} });
 		const workerSpy = jest.spyOn(folderWorker, 'postMessage');
@@ -115,7 +60,7 @@ describe.each<FolderView>(['appointment', 'message', 'contact'])('with %s parame
 		const workerSpy = jest.spyOn(folderWorker, 'postMessage');
 		getSetupServer().use(http.post('/service/soap/GetFolderRequest', handleGetFolderRequest));
 		getSetupServer().use(
-			http.post('/service/soap/GetShareInfoRequest', ({ request }) => {
+			http.post('/service/soap/GetShareInfoRequest', () => {
 				const response = getEmptyMSWShareInfoResponse();
 				return HttpResponse.json(response);
 			})

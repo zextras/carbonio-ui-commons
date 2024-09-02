a3127c551724ab76f1e049f2e5c4b8703854b9af
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-09-02 12:54:21 +0200

    refactor: remove unneccessary try in geFoldersByAccount

--- hooks/use-initialize-folders.tsx
+++ hooks/use-initialize-folders.tsx
@@ -12,27 +12,21 @@ import { getShareInfoRequest } from '../soap/get-share-info';
 import { FolderView } from '../types';
 import { folderWorker } from '../worker';
 
-const getFoldersByAccounts = async (sharedAccounts: unknown[], view: FolderView): Promise<any> => {
-	try {
-		return await Promise.all(
-			map(sharedAccounts, async ({ ownerEmail }: { ownerEmail: string }) => {
-				const response = await getFolderRequest({ view }, ownerEmail);
-				if (response?.folder?.length) {
-					return {
-						...response.folder[0],
-						oname: response.folder[0].name,
-						owner: ownerEmail,
-						name: ownerEmail
-					};
-				}
-				return response;
-			})
-		);
-	} catch (error) {
-		console.error('Error fetching folders by accounts:', error);
-		return [];
-	}
-};
+const getFoldersByAccounts = async (sharedAccounts: unknown[], view: FolderView): Promise<any> =>
+	Promise.all(
+		map(sharedAccounts, async ({ ownerEmail }: { ownerEmail: string }) => {
+			const response = await getFolderRequest({ view }, ownerEmail);
+			if (response?.folder?.length) {
+				return {
+					...response.folder[0],
+					oname: response.folder[0].name,
+					owner: ownerEmail,
+					name: ownerEmail
+				};
+			}
+			return response;
+		})
+	);
 
 export const useInitializeFolders = (view: FolderView): void => {
 	const isLoading = useRef(false);

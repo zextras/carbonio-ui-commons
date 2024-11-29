0674c8250e26e998ad930822ddf1095e2e3f75f1
Davide Frison <davide.frison@zextras.com>
2024-11-29 18:04:21 +0100

    refactor: add common types in carbonio-ui-commons
    - update tests/logic of ContactInput in calendar

---
+++ integrations/constants.ts
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { USER_TYPES } from './types';
+
+export const USER_TYPES_CONST: USER_TYPES = {
+	GROUP: 'CONTACT_GROUP',
+	DISTRIBUTION_LIST: 'DISTRIBUTION_LIST',
+	CONTACT: 'CONTACT'
+};
+export const EDIT_ACTION_ID = 'edit-contact-chip-action';
---
+++ integrations/types.ts
@@ -0,0 +1,38 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { ChipItem } from '@zextras/carbonio-design-system';
+
+type RequiredEmailLabelChipItem<T> = Required<Pick<ChipItem<T>, 'value'>> &
+	Required<Pick<ChipItem<T>, 'label'>> &
+	Omit<ChipItem<T>, 'label' | 'value'>;
+
+export type USER_TYPES = {
+	GROUP: 'CONTACT_GROUP';
+	DISTRIBUTION_LIST: 'DISTRIBUTION_LIST';
+	CONTACT: 'CONTACT';
+};
+
+export type UserContact = {
+	id: string;
+	firstName?: string;
+	middleName?: string;
+	lastName?: string;
+	fullName?: string;
+	company?: string;
+	email: string;
+	type: USER_TYPES['CONTACT'];
+};
+
+export type UserDistributionList = {
+	id: string;
+	email: string;
+	type: USER_TYPES['DISTRIBUTION_LIST'];
+};
+
+export type UserOrDL = UserContact | UserDistributionList;
+
+export type ContactInputItem = RequiredEmailLabelChipItem<UserOrDL>;

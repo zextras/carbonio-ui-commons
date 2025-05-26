b93b8cdac6377e21b81032a9bc9bfc702e784978
Keshav Bhatt <keshavnrj@gmail.com>
2025-05-26 19:36:21 +0530

    chore(jest-setup.ts): standardize import statements and improve code consistency

--- jest-setup.ts
+++ jest-setup.ts
@@ -4,69 +4,67 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import "@testing-library/jest-dom";
+import '@testing-library/jest-dom';
 
-import failOnConsole from "jest-fail-on-console";
-import fetchMock from "jest-fetch-mock";
+import failOnConsole from 'jest-fail-on-console';
+import fetchMock from 'jest-fetch-mock';
 
 import {
-  getFailOnConsoleDefaultConfig,
-  defaultBeforeAllTests,
-  defaultBeforeEachTest,
-  defaultAfterEachTest,
-  defaultAfterAllTests,
-} from "./src/__test__/jest-setup";
-import { useLocalStorage } from "./src/__test__/mocks/carbonio-shell-ui";
+	getFailOnConsoleDefaultConfig,
+	defaultBeforeAllTests,
+	defaultBeforeEachTest,
+	defaultAfterEachTest,
+	defaultAfterAllTests,
+	useLocalStorage
+} from './src';
 
 failOnConsole({
-  ...getFailOnConsoleDefaultConfig(),
-  silenceMessage: (message) =>
-    message.includes(
-      "React does not recognize the `isGeneric` prop on a DOM element",
-    ),
+	...getFailOnConsoleDefaultConfig(),
+	silenceMessage: (message) =>
+		message.includes('React does not recognize the `isGeneric` prop on a DOM element')
 });
 
 beforeAll(() => {
-  fetchMock.doMock();
-  defaultBeforeAllTests();
-  useLocalStorage.mockReturnValue([jest.fn(), jest.fn()]);
+	fetchMock.doMock();
+	defaultBeforeAllTests();
+	useLocalStorage.mockReturnValue([jest.fn(), jest.fn()]);
 });
 
 beforeEach(() => {
-  defaultBeforeEachTest();
+	defaultBeforeEachTest();
 });
 
 afterEach(() => {
-  defaultAfterEachTest();
+	defaultAfterEachTest();
 });
 
 afterAll(() => {
-  defaultAfterAllTests();
+	defaultAfterAllTests();
 });
 
 // Mock matchMedia
 // see: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
-Object.defineProperty(window, "matchMedia", {
-  writable: true,
-  value: jest.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-  })),
+Object.defineProperty(window, 'matchMedia', {
+	writable: true,
+	value: jest.fn().mockImplementation((query) => ({
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: jest.fn(), // deprecated
+		removeListener: jest.fn(), // deprecated
+		addEventListener: jest.fn(),
+		removeEventListener: jest.fn()
+	}))
 });
 
 // Mock implementation of window.open
-Object.defineProperty(window, "open", {
-  writable: true,
-  value: jest.fn(),
+Object.defineProperty(window, 'open', {
+	writable: true,
+	value: jest.fn()
 });
 
 // mock a simplified crypto
-Object.defineProperty(window.crypto, "randomUUID", {
-  writable: true,
-  value: jest.fn(() => Math.random().toString()),
+Object.defineProperty(window.crypto, 'randomUUID', {
+	writable: true,
+	value: jest.fn(() => Math.random().toString())
 });
